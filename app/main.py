7d725b1fed1f57a4fe62331ebadd5a5cf08e5973
gsabatino9 <gsabatino@fi.uba.ar>
2021-12-16 21:06:09 -0300

    Agrego cors

--- app/main.py
+++ app/main.py
@@ -1,11 +1,24 @@
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
-
+from fastapi.middleware.cors import CORSMiddleware
 from .routers import tickets, products
 from .config import settings
 from .db.database import get_db
 
 app = FastAPI(dependencies=[Depends(get_db)])
+
+origins = [
+    "https://squad4-tickets.herokuapp.com/",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(tickets.router)
 app.include_router(products.router)
